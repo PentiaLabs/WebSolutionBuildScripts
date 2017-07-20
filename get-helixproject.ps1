43921cb9b687923b0c6254b6cb1f883fe65c752d
Emil Okkels Klein <eok@pentia.dk>
2017-07-20 13:48:25 +0200

    add get-helixproject Function

--- get-helixproject.ps1
+++ get-helixproject.ps1
@@ -1,4 +1,23 @@
-$path = "D:\Projects\HC"
-$projects = get-childitem -Path $path -Recurse *.csproj 
-$helixProjects = $projects | Select-Object  @{n="FullName"; e={$_.FullName}},@{n="Layer"; e={$_.FullName.Remove(0,$_.FullName.IndexOf("src")+4).Split("\") | select -First 1}}
-$helixProjects 
+Function get-helixproject {
+	[CmdletBinding()]
+	Param(
+		[Parameter(Position = 0)]
+		[string]$Path,
+		[Parameter(Position = 1)]
+		[ValidateSet('Feature', 'Foundation', 'Project')]
+		[string]$Layer)
+	
+	if ([string]::IsNullOrEmpty($Path)) {
+		$path = $PSScriptRoot;
+	}
+	
+	$projects = get-childitem -Path $path -Recurse *.csproj 
+	$helixProjects = $projects | Select-Object  @{n = "FullName"; e = {$_.FullName}}, @{n = "Layer"; e = {$_.FullName.Remove(0, $_.FullName.IndexOf("src") + 4).Split("\") | select -First 1}}
+	
+	$LayerIsEmpty = [string]::IsNullOrEmpty($Layer)
+	if (-not $LayerIsEmpty) {
+		$helixProjects = $helixProjects | Where-Object -Property Layer -EQ $Layer
+	}
+	
+	$helixProjects
+}
