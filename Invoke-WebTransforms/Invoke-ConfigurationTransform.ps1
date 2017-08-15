d0be865b9bb8ed695d13955c2fffae1a3e3d9dd7
Emil Okkels Klein <eok@pentia.dk>
2017-08-15 15:22:47 +0200

    remove msbuild part of XMLTransform, change to Direct call of DLL

--- Invoke-WebTransforms/Invoke-ConfigurationTransform.ps1
+++ Invoke-WebTransforms/Invoke-ConfigurationTransform.ps1
@@ -1,26 +1,91 @@
 . "$PSScriptRoot\..\Shared\Get-MSBuild.ps1"
 
+<#
+.SYNOPSIS
+Short description
+
+.DESCRIPTION
+Given the following file and folder structure:
+    /src/Foundation/MyProject/App_Config/MyProject/MyConfig.Debug.config
+    ... the transform "MyConfig.Debug.config" is applied to the file "/webroot/App_Config/MyProject/MyProject.config" when $BuildConfiguration is "Debug".
+
+.PARAMETER ConfigurationTransformFilePath
+Parameter description
+
+.PARAMETER BuildConfiguration
+Parameter description
+
+.PARAMETER TransformDirectory
+Parameter description
+
+.PARAMETER MSBuildExecutablePath
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
 Function Invoke-ConfigurationTransform {
     [CmdletBinding()]
     Param (
         [Parameter(Position = 0, Mandatory = $True, ValueFromPipeline)]
-        [string]$ConfigurationFilePath,
-        [Parameter(Position = 1, Mandatory = $True)]
-        [string]$BuildConfiguration,
+        [string]$ConfigurationTransformFilePath,
         [Parameter(Position = 2, Mandatory = $True)]
-        [string]$OutputFilePath,
-        [Parameter(Position = 3, Mandatory = $False)]
-        [string]$MSBuildExecutablePath
+        [string]$WebrootDirectory
     )
 
     Process {
-        if ([string]::IsNullOrEmpty($MSBuildExecutablePath)) {
-            Write-Verbose "`$MSBuildExecutablePath not set."
-            $MSBuildExecutablePath = Get-MSBuild
-        }
-        Write-Verbose "Using '$MSBuildExecutablePath'."
-        Write-Verbose "Transforming '$ConfigurationFilePath' to '$OutputFilePath' for build configuration '$BuildConfiguration'."
-        & "$MSBuildExecutablePath" "./ApplyTransform.targets" /target:"ApplyTransform" /property:Configuration="$BuildConfiguration" /p:WebConfigToTransform="$OutputFilePath" /p:TransformFile="$ConfigurationFilePath" /p:FileToTransform="$($_.Name)"
+        $FileToTransform = Get-FileToTransform -ConfigurationTransformFilePath $ConfigurationTransformFilePath
+        $FileToTransform = [System.IO.Path]::Combine($WebrootDirectory, $FileToTransform)
+
+        Write-Verbose "Transforming '$ConfigurationTransformFilePath' to '$FileToTransform'."
+        
+        XmlDocTransform -xml $FileToTransform -xdt $ConfigurationTransformFileName
+    }
+}
+
+Function Get-FileToTransform {
+    Param (
+        [Parameter(Mandatory = $True)]
+        [string]$ConfigurationTransformFilePath
+    )
+
+    #TODO Add handling of App_Config 
+
+    $BuildConfiguration = $ConfigurationTransformFilePath.Split(".") | Select-Object -Last 2 | Select-Object -First 1 
+
+    $BuildConfigurationLenght = $BuildConfiguration.Length
+    
+    $ConfigurationTransformFileName = Get-Item $ConfigurationTransformFilePath | Select-Object -ExpandProperty Name
+
+    $IndexOfBuildConfiguration = $ConfigurationTransformFileName.LastIndexOf($BuildConfiguration)
+
+    return $ConfigurationTransformFileName.Remove($IndexOfBuildConfiguration, $BuildConfigurationLenght+1)
+}
+
+Function XmlDocTransform($xml, $xdt)
+{
+    if (!$xml -or !(Test-Path -path $xml -PathType Leaf)) {
+        throw "File not found. $xml";
+    }
+    if (!$xdt -or !(Test-Path -path $xdt -PathType Leaf)) {
+        throw "File not found. $xdt";
     }
 
+    Add-Type -LiteralPath "$PSScriptRoot\Microsoft.Web.XmlTransform.dll"
+
+    $xmldoc = New-Object Microsoft.Web.XmlTransform.XmlTransformableDocument;
+    $xmldoc.PreserveWhitespace = $true
+    $xmldoc.Load($xml);
+
+    $transf = New-Object Microsoft.Web.XmlTransform.XmlTransformation($xdt);
+    if ($transf.Apply($xmldoc) -eq $false)
+    {
+        throw "Transformation failed."
+    }
+    $xmldoc.Save($xml);
 }
+
+Invoke-ConfigurationTransform -ConfigurationTransformFilePath D:\Projects\AAB.Intranet\src\Project\Environment\App_Config\Include\dataFolder.Debug.config -WebrootDirectory D:\websites\AAB.Intranet -Verbose
