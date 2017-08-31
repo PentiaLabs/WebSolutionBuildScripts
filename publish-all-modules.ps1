69f5975fab1221aa27d1193c619460a296285ba2
Emil Okkels Klein <eok@pentia.dk>
2017-08-31 13:28:26 +0200

    add check if Tund exists as PSRepository

--- publish-all-modules.ps1
+++ publish-all-modules.ps1
@@ -1,1 +1,8 @@
+$numberOfRepositoriesNamedTund = Get-PSRepository Tund | measure | Select-Object -ExpandProperty	Count 
+
+if($numberOfRepositoriesNamedTund -lt 1)
+{
+	Register-PSRepository -Name Tund -SourceLocation http://tund/nuget/powershell/ -PublishLocation http://tund/nuget/powershell/
+}
+
 get-module -Name .\**\*.psd1 -ListAvailable | Select-Object -ExpandProperty ModuleBase | % { Publish-Module -Path $_ -NuGetApiKey ***REMOVED*** -Repository Tund -Force }
