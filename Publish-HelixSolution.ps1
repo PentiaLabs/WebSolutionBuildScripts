d143638888472576581fd6c66f22f03e4395f62a
Uli Weltersbach <uw@pentia.dk>
2017-08-31 09:54:12 +0200

    Moved "Get-RuntimeDependency" to separate module
    #2

--- Publish-HelixSolution.ps1
+++ Publish-HelixSolution.ps1
@@ -1,6 +1,9 @@
+Import-Module "$PSScriptRoot\Publish-RuntimeDependencyPackage\Get-RuntimeDependencyPackage.ps1" -Force
+Import-Module "$PSScriptRoot\Publish-RuntimeDependencyPackage\Publish-RuntimeDependencyPackage.ps1" -Force
+
 Import-Module "$PSScriptRoot\Publish-HelixSolution\Get-SitecoreHelixProject.psm1" -Force
 Import-Module "$PSScriptRoot\Publish-HelixSolution\Publish-WebProject.psm1" -Force
-Import-Module "$PSScriptRoot\Publish-RuntimeDependencyPackage\Publish-RuntimeDependencyPackage.ps1" -Force
+
 Import-Module "$PSScriptRoot\Invoke-ConfigurationTransform\Invoke-ConfigurationTransform.psm1" -Force
 
 Function Publish-HelixSolution {
@@ -30,81 +33,4 @@ Function Publish-HelixSolution {
     # 3. Publish all web projects
 
     # 4. Invoke configuration transforms
-}
-
-<#
-.SYNOPSIS
-Gets all NuGet package references from a well formed NuGet packages.config-file.
-
-.DESCRIPTION
-See https://docs.microsoft.com/en-us/nuget/schema/packages-config for the correct file format.
-Only the "id" and "version" attributes are used.
-
-.PARAMETER ConfigurationFilePath
-The path of the packages.config file.
-
-.EXAMPLE
-Get-RuntimeDependency -ConfigurationFilePath my-solution\runtime-dependencies.config
-
-Contents of "runtime-dependencies.config":
-<?xml version="1.0" encoding="utf-8"?>
-<packages>
-  <package id="jQuery" version="3.1.1" />
-  <package id="NLog" version="4.3.10" />
-</packages>
-
-Returns:
-[{id:"jQuery",version:"3.1.1"},{id:"NLog",version:"4.3.10"}]
-
-#>
-Function Get-RuntimeDependency {
-    Param(
-        [Parameter(Mandatory = $True)]
-        [string]$ConfigurationFilePath
-    )
-
-    If (!(Test-Path $ConfigurationFilePath -PathType Leaf)) {
-        $message = "File '$ConfigurationFilePath' not found."
-        $argumentException = (New-Object "System.ArgumentException" $message, $_.Exception)
-        Throw $argumentException
-    }
-
-    $configuration = Get-PackageConfiguration -ConfigurationFilePath $ConfigurationFilePath
-
-    If (!($configuration.packages)) {
-        Throw "No 'packages' root element found in '$ConfigurationFilePath'. Run 'Get-Help Get-RuntimeDependency -Full' for expected usage."
-    }
-
-    $packages = $configuration.packages.package | Select-Object -Property "id", "version"
-    Write-Verbose "Found $($packages.Count) package(s) in '$ConfigurationFilePath'."
-    $packages
-}
-
-Function Get-PackageConfiguration {
-    Param(
-        [Parameter(Mandatory = $True)]
-        [string]$ConfigurationFilePath
-    )
-
-    Try {
-        [xml]$configuration = Get-Content -Path $ConfigurationFilePath
-    }
-    Catch [System.Management.Automation.RuntimeException] {
-        If (Test-XmlParseException $_.Exception) {
-            $message = "File '$ConfigurationFilePath' isn't valid XML. Run 'Get-Help Get-RuntimeDependency -Full' for expected usage."
-            $argumentException = (New-Object "System.ArgumentException" $message, $_.Exception)
-            Throw $argumentException
-        }
-        Throw $_.Exception
-    }
-
-    $configuration
-}
-
-Function Test-XmlParseException {
-    Param(
-        [Parameter(Mandatory = $True)]
-        [System.Management.Automation.RuntimeException]$Exception
-    )
-    $Exception.Message -match "Cannot convert value .* to type ""System\.Xml\.XmlDocument""\."
 }
---
+++ Publish-RuntimeDependencyPackage/Get-RuntimeDependencyPackage.Tests.ps1
@@ -0,0 +1,62 @@
+# Requires https://github.com/pester/Pester: Install-Module Pester -Force -SkipPublisherCheck
+Import-Module "$PSScriptRoot\Get-RuntimeDependencyPackage.psm1" -Force
+
+Describe "Get-RuntimeDependencyPackage" {
+    It "should throw an exception if the file doesn't exist" {
+        # Arrange
+        $runtimeDependencyConfigurationFilePath = "$TestDrive\test-configuration-file.config"
+
+        # Act
+        $invocation = { Get-RuntimeDependencyPackage -ConfigurationFilePath $runtimeDependencyConfigurationFilePath }
+
+        # Assert
+        $invocation | Should Throw "File '$runtimeDependencyConfigurationFilePath' not found."
+    }
+    
+    It "should throw an exception if the file isn't valid XML" {
+        # Arrange
+        $runtimeDependencyConfigurationFilePath = "$TestDrive\test-configuration-file.config"
+        Set-Content -Path $runtimeDependencyConfigurationFilePath -Value "This isn't XML"
+    
+        # Act
+        $invocation = { Get-RuntimeDependencyPackage -ConfigurationFilePath $runtimeDependencyConfigurationFilePath }
+    
+        # Assert
+        $invocation | Should Throw "File '$runtimeDependencyConfigurationFilePath' isn't valid XML. Run 'Get-Help Get-RuntimeDependencyPackage -Full' for expected usage."
+    }
+
+    It "should throw an exception if the 'packages' element doesn't exist" {
+        # Arrange
+        $runtimeDependencyConfigurationFilePath = "$TestDrive\test-configuration-file.config"
+        Set-Content -Path $runtimeDependencyConfigurationFilePath -Value "<root></root>"
+
+        # Act
+        $invocation = { Get-RuntimeDependencyPackage -ConfigurationFilePath $runtimeDependencyConfigurationFilePath }
+
+        # Assert
+        $invocation | Should Throw "No 'packages' root element found in '$runtimeDependencyConfigurationFilePath'. Run 'Get-Help Get-RuntimeDependencyPackage -Full' for expected usage."
+    }
+
+    It "should return all package references found in the configuration file" {
+        # Arrange
+        $runtimeDependencyConfigurationFilePath = "$TestDrive\test-configuration-file.config"
+        $packagesConfig = '<?xml version="1.0" encoding="utf-8"?>
+<packages>
+    <package id="jQuery" version="3.1.1" />
+    <package id="NLog" version="4.3.10" />
+</packages>'
+        Set-Content -Path $runtimeDependencyConfigurationFilePath -Value $packagesConfig
+
+        # Act
+        $runtimeDependencies = Get-RuntimeDependencyPackage -ConfigurationFilePath $runtimeDependencyConfigurationFilePath
+        $jQuery = $runtimeDependencies | Select-Object -First 1
+        $NLog = $runtimeDependencies | Select-Object -Last 1
+
+        # Assert
+        $runtimeDependencies.Count | Should Be 2
+        $jQuery.id | Should Be "jQuery"
+        $jQuery.version | Should Be "3.1.1"
+        $NLog.id | Should Be "NLog"
+        $NLog.version | Should Be "4.3.10"
+    }
+}
