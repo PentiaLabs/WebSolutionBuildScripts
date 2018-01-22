1ede44999de1582ecd4af54983f99cff8c1bd901
Uli Weltersbach <uli.weltersbach@gmail.com>
2018-01-22 09:30:26 +0100

    Issue 117/psd vs psm (#118)
    * Partial sanity check implementation
    
    Currently there's an issue reg. checks of psd1 vs psm1 exports. It might prove necessary to fall back to string parsing and comparison.
    
    #117
    
    * Changed psm vs psd test to avoid module imports
    
    Even though regex and file content comparison seems less robust, this method currently catches spelling mistakes in exported function names, which the module-based approach doesn't.
    
    Resolves #117

---
+++ ModuleMemberExport.Tests.ps1
@@ -0,0 +1,58 @@
+Describe "module member exports" {
+    $moduleDefinitionFiles = Get-ChildItem -Path "$PSScriptRoot" -Include "*.psd1" -Recurse
+
+    foreach ($moduleDefinitionFile in $moduleDefinitionFiles) {
+
+        Describe "'$($moduleDefinitionFile.Name)' member export" {
+
+            Function Get-ExportedCommandsFromDefinition {
+                Param(
+                    [Parameter(Mandatory = $True)]
+                    [String]$ModuleDefinitionFile
+                )
+                $content = (Get-Content $ModuleDefinitionFile | Out-String)
+                $moduleFromDefinition = (Invoke-Expression $content)
+                $exportedCommandsFromDefinition = $moduleFromDefinition.FunctionsToExport
+                $exportedCommandsFromDefinition
+            }
+
+            Function Get-ExportedCommandsFromScript {
+                Param(
+                    [Parameter(Mandatory = $True)]
+                    [String]$ModuleDefinitionFile
+                )
+                $moduleScriptFilePath = "$ModuleDefinitionFile".Replace("psd1", "psm1")      
+                $moduleScriptFileContent = Get-Content $moduleScriptFilePath -Raw 
+                $exportModuleMemberRegex = [Regex]"(?mi)^Export-ModuleMember -Function (.*)$"
+                $exportedCommandsFromScriptMatches = $exportModuleMemberRegex.Matches($moduleScriptFileContent)
+                $exportedCommandsFromScript = $exportedCommandsFromScriptMatches[0].Groups[1].Value.Split(",") | ForEach-Object { $_.Trim() }
+                $exportedCommandsFromScript
+            }
+
+            It "should export the same members from the .psm1 as from the .psd1 file" {
+                # Arrange
+                $exportedCommandsFromDefinition = Get-ExportedCommandsFromDefinition -ModuleDefinitionFile $moduleDefinitionFile
+                $exportedCommandsFromScript = Get-ExportedCommandsFromScript -ModuleDefinitionFile $moduleDefinitionFile
+
+                # Act
+                $missingExports = $exportedCommandsFromDefinition | Where-Object { $exportedCommandsFromScript -notcontains $_ }
+
+                # Assert
+                $missingExports | Should Be $Null
+            }
+
+            It "should export the same members from the .psd1 as from the .psm1 file" {
+                # Arrange
+                $exportedCommandsFromDefinition = Get-ExportedCommandsFromDefinition -ModuleDefinitionFile $moduleDefinitionFile
+                $exportedCommandsFromScript = Get-ExportedCommandsFromScript -ModuleDefinitionFile $moduleDefinitionFile
+
+                # Act            
+                $missingExports = $exportedCommandsFromScript | Where-Object { $exportedCommandsFromDefinition -notcontains $_ }
+
+                # Assert
+                $missingExports | Should Be $Null
+            }
+        }
+
+    }
+}
